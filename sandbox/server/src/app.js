0437746a2de450b66ec75c313313473bcb85fde1
kambleaditya490@gmail.com <kambleaditya490@gmail.com>
2026-06-14 09:11:49 +0530

    pushing code

---
+++ sandbox/server/src/app.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/api/sandbox/health', (req, res) => {
+    res.json({ status: 'ok' }); 
+});
+
+export default  app
+
+
